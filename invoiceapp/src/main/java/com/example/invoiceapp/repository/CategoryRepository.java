83712544c3ef7914769e4c6f3aece2ca22ed1e78
pratikdevelop <pratikraut88895@gmail.com>
2025-05-07 22:36:38 +0530

    update

--- invoiceapp/src/main/java/com/example/invoiceapp/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.invoiceapp.repository;
-
-import com.example.invoiceapp.model.Category;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CategoryRepository extends MongoRepository<Category, String> {
-}
--- invoiceapp/src/main/java/com/example/invoiceapp/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.invoiceapp.repository;
-
-import com.example.invoiceapp.model.Customer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CustomerRepository extends MongoRepository<Customer, String> {
-}
